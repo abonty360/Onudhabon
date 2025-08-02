b2413eee5ea339e9c99aa07bb6b560a4b71caa8b
abonty360 <abontyrahman45@gmail.com>
2025-08-02 21:38:34 +0600

    Navigation Bar Added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { useState } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage/LoginPage';
 import RegistrationPage from './Pages/RegistrationPage/RegistrationPage';
 import HomePage from './Pages/HomePage/HomePage';
+import AboutPage from './Pages/AboutPage/AboutPage';
+import LecturePage from './Pages/LecturePage/LecturePage';
+import MaterialPage from './Pages/MaterialPage/MaterialPage';
+import ProfilePage from './Pages/ProfilePage/ProfilePage';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 function App() {
   
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegistrationPage />} />
       <Route path="/home" element={<HomePage />} />
+      <Route path="/lecture" element={<LecturePage />} />
+      <Route path="/material" element={<MaterialPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/about" element={<AboutPage />} />
     </Routes>
   );
 }
---
+++ src/Pages/MaterialPage/MaterialPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavbarComponent from '../../Components/NavbarComp/Navbarcomp';
+function MaterialPage() {
+  return (
+    <div className="material-page">
+      <NavbarComponent />
+    </div>
+  );
+
+}
+export default MaterialPage;
