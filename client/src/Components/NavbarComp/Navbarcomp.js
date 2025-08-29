320476a73c5698b12b860f875173117b05a2100c
abonty360 <abontyrahman45@gmail.com>
2025-08-29 21:15:41 +0600

    latest stable

--- client/src/Components/NavbarComp/Navbarcomp.js
+++ client/src/Components/NavbarComp/Navbarcomp.js
@@ -1,64 +1,46 @@
-import React, { Component } from "react";
-import { Navbar, Nav, Container, Form, Button, Image } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import logo from "../../assets/navbar_logo.png";
-import "./Navbarcomp.css";
+import React, { Component } from 'react'
+import { Navbar, Nav, Container, Form, Button, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/navbar_logo.png'
+import './Navbarcomp.css';
+
 
 export default class NavbarComponent extends Component {
-  render() {
-    return (
-      <Navbar
-        bg="white"
-        expand="lg"
-        fixed="top"
-        className="shadow-sm custom-navbar"
-      >
-        <Container fluid>
-          <Navbar.Brand
-            as={Link}
-            to="/"
-            className="d-flex align-items-center gap-2"
-          >
-            <Image src={logo} alt="Onudhabon Logo" height="60" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="main-navbar" />
-          <Navbar.Collapse id="main-navbar">
-            <Nav className="me-auto mx-4">
-              <Nav.Link as={Link} to="/home">
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="/lecture">
-                Lectures
-              </Nav.Link>
-              <Nav.Link as={Link} to="/material">
-                Materials
-              </Nav.Link>
-              <Nav.Link as={Link} to="/about">
-                About
-              </Nav.Link>
-              <Nav.Link as={Link} to="/forum">
-                Forum
-              </Nav.Link>
-            </Nav>
-            <Form className="d-flex align-items-center gap-2">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button
-                variant="primary"
-                as={Link}
-                to="/login"
-                className="custom-login-btn"
-              >
-                <i className="bi bi-box-arrow-in-right me-1"></i> Login
-              </Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
+    render() {
+        return (
+            <Navbar bg="white" expand="lg" fixed="top" className="shadow-sm custom-navbar">
+                <Container fluid>
+                    <Navbar.Brand as={Link} to="/" className="d-flex align-items-center gap-2">
+                        <Image src={logo} alt="Onudhabon Logo" height="60" />
+
+
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="main-navbar" />
+                    <Navbar.Collapse id="main-navbar">
+                        <Nav className="me-auto mx-4">
+                            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/lecture">Lectures</Nav.Link>
+                            <Nav.Link as={Link} to="/material">Materials</Nav.Link>
+                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        </Nav>
+                        <Form className="d-flex align-items-center gap-2">
+                            <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <Button variant="primary"
+                                as={Link}
+                                to="/login"
+                                className="custom-login-btn"
+                            >
+                                <i className="bi bi-box-arrow-in-right me-1"></i> Login
+                            </Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        )
+    }
 }
