b437be01e05c982c5d2aa7de8bb3c0efa49fec97
Nusrat Jahan Tuli <163337050+tuwuli07@users.noreply.github.com>
2025-08-29 21:20:29 +0600

    Merge pull request #14 from abonty360/materials
    Filter Added in Lecture & Materials page

--- server/models/Material.js
+++ server/models/Material.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
 const materialSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  subject: String,
-  topic: String,
-  classLevel: String,
-  author: String,
-  fileUrl: String,
+  title: { type: String, required: true, trim: true },
+  description: { type: String, required: true, trim: true },
+  instructor: { type: String, required: true, trim: true },
+  version: { type: String, required: true, enum: ["Bangla", "English"] },
+  classLevel: { type: Number, required: true }, 
+  subject: { type: String, required: true },
+  topic: { type: String, required: true },
+  fileUrl: { type: String, required: true },
   size: Number,
   downloads: { type: Number, default: 0 },
   date: { type: Date, default: Date.now }
