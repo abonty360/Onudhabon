5307901038a0a33a68411ee0dd72e4230f2654c3
heyazahin <zahinzia781@gmail.com>
2025-08-30 13:26:53 +0600

    spellFix

--- client/src/Pages/ForumPage/ForumList.jsx
+++ client/src/Pages/ForumPage/ForumList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import ForumCard from "../../Components/ForumCopm/ForumCard";
+import ForumCard from "../../Components/ForumComp/ForumCard";
 import { Button, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
