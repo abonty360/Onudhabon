8ea0d1e1d41b833fdacecf9b5eff5986ff635bd9
Nusrat Jahan Tuli <nusrattuli69@gmail.com>
2025-09-05 12:05:54 +0600

    profile page created with functional bio

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { login, register } from "../controllers/userController.js";
+import { login, register, getProfile, updateProfile } from "../controllers/userController.js";
+import { auth } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 
 router.post("/register", register);
+
+router.get("/profile", auth, getProfile);
+
+router.put("/profile", auth, updateProfile);
 
 export default router;
